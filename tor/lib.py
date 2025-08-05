95a9c574c9d5794b1cfcd3c9a38210115435e3a9
RbCaVi <robert@vail-family.com>
2025-08-04 23:40:18 -0400

    (tor/netdoc) make a Line class :)

--- tor/lib.py
+++ tor/lib.py
@@ -21,27 +21,6 @@ class Peekable:
 def generator_to_list(f):
   return lambda *args, **kwargs: [*f(*args, **kwargs)]
 
-def get_line_args_optional_no_object(line_type, lines, default = None):
-  # assumes lines is a Peekable
-  # returns the line arguments or None
-  # errors if an object is present
-  if lines.peek()[0] == line_type:
-    line = next(lines)
-    assert line[2] is None, f'line of type {line_type} should not have an object'
-    return line[1]
-  else:
-    return default
-
-def get_line_args_no_object(line_type, lines):
-  # lines is a Peekable
-  # returns the line arguments
-  # errors if an object is present
-  check_type = lines.peek()[0]
-  assert check_type == line_type, f'missing required line of type {line_type}; got {check_type}'
-  line = next(lines)
-  assert line[2] is None, f'line of type {line_type} should not have an object'
-  return line[1]
-
 # convert a list of 'key=value' strings to a dictionary
 def params_to_dict(params):
   if params is None:
--- tor/netdoc.py
+++ tor/netdoc.py
@@ -3,6 +3,13 @@ import base64
 
 import lib
 
+class Line:
+  def __init__(self, keyword, arguments, object_name, object_data):
+    self.keyword = keyword
+    self.arguments = arguments
+    self.object_name = object_name
+    self.object_data = object_data
+
 @lib.generator_to_list
 def parse_netdoc(netdoc):
   lines = lib.Peekable(line for line in netdoc.split('\n') if line != '')
@@ -21,4 +28,25 @@ def parse_netdoc(netdoc):
     else:
       object_name = None
       object_data = None
-    yield keyword, arguments, object_name, object_data
+    yield Line(keyword, arguments, object_name, object_data)
+
+def get_line_args_optional_no_object(line_type, lines, default = None):
+  # assumes lines is a Peekable
+  # returns the line arguments or None
+  # errors if an object is present
+  if lines.peek().keyword == line_type:
+    line = next(lines)
+    assert line.object_name is None, f'line of type {line_type} should not have an object'
+    return line.arguments
+  else:
+    return default
+
+def get_line_args_no_object(line_type, lines):
+  # lines is a Peekable
+  # returns the line arguments
+  # errors if an object is present
+  check_type = lines.peek().keyword
+  assert check_type == line_type, f'missing required line of type {line_type}; got {check_type}'
+  line = next(lines)
+  assert line.object_name is None, f'line of type {line_type} should not have an object'
+  return line.arguments
