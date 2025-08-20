8d096d5b5a72762fa4a3f62194f145c76cbdd98e
RbCaVi <robert@vail-family.com>
2025-08-19 21:27:37 -0400

    (tor/tor_dirs) hotel wifi <3

--- tor/tor_dirs.py
+++ tor/tor_dirs.py
@@ -18,6 +18,11 @@ auth_dirs = [ # the contents of https://gitlab.torproject.org/tpo/core/tor/-/blo
 # dizum not blocked
 # dannenberg not blocked
 
+# on hotel wifi:
+# serge blocked
+# moria1 not blocked
+# dizum blocked
+
 fallback_dirs = [ # contents of https://gitlab.torproject.org/tpo/core/tor/-/blob/main/src/app/config/fallback_dirs.inc - but reformatted - i'm not using these this time either <3
   "Quetzalcoatl ipv6=[2a12:a800:2:1:45:141:215:63]:9100 45.141.215.63:9100 82CE42D04B5BA616E611E4758861C864B2ACFCA8",
   "torfinity ipv6=[2001:41d0:701:1100::6e02]:9001 162.19.252.175:443 1734430C503EE26EC1A3B9EB7C29BF4AC64B922B",
