23dd60e5a05ab2e883618dec80bead0630907197
RbCaVi <robert@vail-family.com>
2025-11-19 14:35:14 -0500

    (mod2d) add a motion value to blocks

--- mod2d.py
+++ mod2d.py
@@ -13,25 +13,50 @@
 
 import dataclasses
 import sys
+import enum
+import random
 
 import pygame
 
-#class Moving(enum.Enum):
-#  NONE = enum.auto()
+@dataclasses.dataclass
+class MotionData:
+  moving: bool
+  dx: int
+  dy: int
+
+class Motion(MotionData, enum.Enum):
+  NONE = False, 0, 0
+  UP = True, 0, -1
+  DOWN = True, 0, 1
+  LEFT = True, -1, 0
+  RIGHT = True, 1, 0
 
 @dataclasses.dataclass
 class Block:
   blockid: int
+  motion: Motion
 
 blockids = [
-  [0, 1, 2],
-  [1, 1, 2],
-  [0, 1, 1],
+  [0, 1, 2, 0, 0, 0, 0, 0],
+  [1, 1, 2, 0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+colors = [
+  (  0,   0,   0),
+  (  0, 128, 128),
+  (100, 128, 128),
+  (200, 128, 128),
+  (0, 0, 0),
+  (0, 0, 0),
 ]
 
 blocks = [
   [
-    Block(blockid)
+    Block(blockid, random.choice([*Motion.__members__.values()]))
     for blockid in row
   ]
   for row in blockids
@@ -53,6 +78,8 @@ while True:
     for xi,block in enumerate(row):
       x = blocksize * xi
       y = blocksize * yi
-      pygame.draw.rect(display, (100 * block.blockid, 128, 128), (x, y, blocksize, blocksize))
+      pygame.draw.rect(display, colors[block.blockid], (x, y, blocksize, blocksize))
+      pygame.draw.rect(display, (255, 255, 255), (x, y, blocksize, blocksize), width = 1)
+      pygame.draw.rect(display, (255, 255, 255), (x + blocksize / 2 - 5 + 15 * block.motion.dx, y + blocksize / 2 - 5 + 15 * block.motion.dy, 10, 10))
   pygame.display.update()
   clock.tick(60)
